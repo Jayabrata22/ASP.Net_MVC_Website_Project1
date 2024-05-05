8ea9f9d5e648dfe663ac752cee7b3a5f7b8895b8
[Jayabrata Das] <[dhyanabratad22@gmail.com]>
2024-05-05 15:10:25 +0530

    Add project files.

---
+++ Website1/Controllers/ServiceController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Website1.Controllers
+{
+    public class ServiceController : Controller
+    {
+        public IActionResult Service()
+        {
+            return View();
+        }
+    }
+}
